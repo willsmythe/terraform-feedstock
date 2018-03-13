558f14448a21d5b028d47355039568adce8bab23
Patrick Sodré <sodre@sodre.co>
2018-03-13 01:25:15 -0400

    Build from source for Linux and OSX

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,17 @@
-#!bin/bash
+#!/usr/bin/env bash
 
-mkdir -p $PREFIX/bin
-chmod +x $PKG_NAME
-cp $PKG_NAME $PREFIX/bin
+# Create temporary GOPATH
+mkdir build
+export GOPATH=$(pwd)/build
+mkdir -p $GOPATH/src/github.com/terraform-providers
+
+# Link code to GOPATH directory
+ln -s $(pwd) $GOPATH/src/github.com/terraform-providers/$PKG_NAME
+
+# Build
+cd $GOPATH/src/github.com/terraform-providers/$PKG_NAME
+make build
+make test
+
+# Install Binary into PREFIX/bin
+mv $GOPATH/bin/$PKG_NAME $PREFIX/bin/${PKG_NAME}_v${PKG_VERSION}_x4
